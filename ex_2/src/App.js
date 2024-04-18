36647faf64a2cb6c0ad545622610cdba985a66dd
nanami-tomoe <npnp123npnp123@gmail.com>
2024-04-19 04:19:24 +0900

    Practice #2 Class Component

---
+++ ex_2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hi from './component/MyClass';
+
+function App() {
+  return (
+    <div className="App">
+      <Hi />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex_2/src/component/MyClass.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MyClass extends React.Component {
+  render() {
+    return (<h1>Hi everyone!</h1>);
+  }
+}
+
+export default MyClass;
